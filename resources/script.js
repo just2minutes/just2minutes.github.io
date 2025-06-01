dab6c44cac8272ac1cee6b28fc0c225b2153188b
just2minutes <zalmantzvi@gmail.com>
2025-06-01 14:34:17 +0300

    e

--- resources/script.js
+++ resources/script.js
@@ -66,8 +66,7 @@ function renderVideos(videos) {
             <p>${video.title}</p>
             <iframe 
                 width="360" height="203"
-                src="https://www.youtube.com/embed/${video.videoId}modestbranding=1&showinfo=0&autoplay=0"
-                loading="lazy"
+                src="https://www.youtube.com/embed/${video.videoId}rel=0&modestbranding=1&showinfo=0&autoplay=0"
                 title="${video.title}" 
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
