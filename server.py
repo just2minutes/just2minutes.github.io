f576e23f97aa2ee119e2080b3008e0516af1845c
just2minutes <zalmantzvi@gmail.com>
2025-06-24 15:10:52 +0300

    s

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import http.server
-import socketserver
+#import http.server
+#import socketserver
 
-PORT = 1000
+#PORT = 1000
 
-Handler = http.server.SimpleHTTPRequestHandler
+#Handler = http.server.SimpleHTTPRequestHandler
 
-if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), Handler) as http:
-        print(f"Serving at http://localhost:{PORT}")
-        http.serve_forever()
+#if __name__ == "__main__":
+#    with socketserver.TCPServer(("", PORT), Handler) as http:
+#        print(f"Serving at http://localhost:{PORT}")
+#        http.serve_forever()
